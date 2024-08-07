d4f0b22befc548b9d53a629b50d7167c4b899f23
Lewis Wilkie <lewis.wilkie@niwa.co.nz>
2024-08-07 12:05:58 +1200

    install docker script

---
+++ install_das-ui.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+
+function install_das-ui_docker {
+    echo Removing previous DAS-UI docker container and images
+    sudo docker stop das-ui || true && sudo docker rm das-ui || true
+    sudo docker network create --subnet=172.18.0.0/16 das-net || true
+    sudo docker image prune --all -f --filter label=openrvdas="das-ui"
+    sudo docker volume prune --all -f --filter label=openrvdas="das-ui"
+    sudo docker build -t openrvdas/das-ui-image -f local/niwa/utils/dockerfile_das-ui .
+    sudo docker run -d -p 3000:80 --restart=always --name=das-ui --net das-net --ip 172.18.0.7 openrvdas/das-ui-image
+}
+
+install_das-ui_docker
+
+
+echo "#########################################################################"
+echo "Installation complete"
+echo "DAS-UI server available at localhost:80"
+echo
