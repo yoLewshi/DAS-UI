7f196f04ef6d7aecb9ee35e046095ce7b52ba69d
Lewis Wilkie <lewis.wilkie@niwa.co.nz>
2024-08-30 12:57:53 +1200

    dark style tweaks

--- src/shared_components/yaml_editor/index.jsx
+++ src/shared_components/yaml_editor/index.jsx
@@ -1,5 +1,6 @@
-import React, {useContext, useState} from 'react';
-import {DiffEditor} from '@monaco-editor/react';
+import React, {useContext, useEffect, useState} from 'react';
+import {useMonaco, DiffEditor} from '@monaco-editor/react';
+
 import { GlobalContext } from '../../shared_components/globalContext';
 
 import Loader from '../loader';
@@ -22,6 +23,11 @@ function YamlEditor(props) {
         renderSideBySide: false
     };
 
+    const themeMap = {
+        "default": "vs",
+        "dark": "monokai"
+    }
+
     const editorOptions = Object.assign(defaultEditorOptions, props.editorOptions || {});
 
     function updateChanges(e) {
@@ -38,13 +44,26 @@ function YamlEditor(props) {
         component.onDidUpdateDiff(updateChanges);
     }
 
-    
+    const monaco = useMonaco();
+
+    useEffect(() => {
+        if (monaco) {
+            Promise.all([
+                import('../../style/monaco_themes/monokai.json'),
+            ]).then(([monokai]) => {
+                monaco.editor.defineTheme('monokai', monokai);
+                monaco.editor.setTheme(themeMap[global.theme] || "vs");
+
+            }).catch(e => console.error(e));
+        }
+    }, [monaco, global.theme]);
+
     return (
         <>
             <div className={cx(["change_label"])}>Changes made: <b>{changes.length}</b></div>
             <DiffEditor
                 language="yaml"
-                theme={global.theme == "default" ? "vs" : "vs-dark"}
+                theme={themeMap[global.theme] || "vs"}
                 loading={<Loader />}
                 original={fileContent}
                 modified={fileContent}
