688834c2ca9bfac17fa8b9814dfa1774c39eb75c
Lewis Wilkie <lewis.wilkie@niwa.co.nz>
2024-09-02 23:19:21 +1200

    safer docker

--- install_das-ui.sh
+++ install_das-ui.sh
@@ -4,10 +4,10 @@ function install_das-ui_docker {
     echo Removing previous DAS-UI docker container and images
     sudo docker stop das-ui || true && sudo docker rm das-ui || true
     sudo docker network create --subnet=172.18.0.0/16 das-net || true
-    sudo docker image prune --all -f --filter label=openrvdas="das-ui"
-    sudo docker volume prune --all -f --filter label=openrvdas="das-ui"
     sudo docker build -t openrvdas/das-ui-image -f dockerfile_das-ui .
     sudo docker run -d -p 80:3000 --restart=always --name=das-ui --net das-net --ip 172.18.0.7 openrvdas/das-ui-image
+    sudo docker image prune --all -f --filter label=openrvdas="das-ui"
+    sudo docker volume prune --all -f --filter label=openrvdas="das-ui"
 }
 
 install_das-ui_docker
